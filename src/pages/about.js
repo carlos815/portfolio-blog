546c3c11d5067439088717c95aa4450dac6db7dd
carlos815 <carlos_t815@outlook.com>
2023-01-24 14:42:37 -0500

    Added about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,26 +1,23 @@
 import * as React from "react"
 import { graphql } from "gatsby"
-import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Section from "../components/section"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
-  const posts = data.allMarkdownRemark.nodes
 
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
+  const myAge = () => {
+    const myBirthDay = new Date("February 8, 1993 04:20:00")
+    const now = new Date()
+    const a = new Date(now - myBirthDay)
+    return (a.getFullYear() - 1970)
   }
+
+  React.useEffect(() => {
+    myAge()
+  }, [])
+
 
   return (
     <Layout location={location} title={siteTitle}>
@@ -28,18 +25,48 @@ const BlogIndex = ({ data, location }) => {
       <Section>
         <h1>Carlos Hernández? Who that?</h1>
 
-        <p className="text-base mb-4">Hi! I'm Carlos. </p>
-        <p className="text-base mb-4">
-          I'm a developer in Colombia. I make programming projects and then write about them. I am constantly learning things. I make music, practice chess, play videogames, write and read a lot.
+        <p className="">I'm a developer from Venezuela. I'm currently based in Colombia. I'm {myAge()} years old.</p>
+
+        <p className=" text-comment">
+          //I was actually an economist, but pivoted to development during the pandemic
         </p>
-        <h2>My projects</h2>
-        <p className="text-base mb-4">
-          The list of projects goes here
-        </p>
-        <h2>My publications</h2>
-        <p className="text-base mb-4">
-          The list of publications goes here.
-        </p>
+
+        <p>I have worked with React, Flutter and even Python for some data analysis, but I mostly gravitate towards React.</p>
+        <p>I&#39;m the creator of <a href="laslucas.com">Las Lucas</a>, a completely free website that compiles the different exchange rates in Venezuela. It has over 10.000 visits each month.</p>
+        <p>The site uses multiple web scrappers to keep itself updated with the latest price on the many <em>officialish</em> exchange rates (long story) and shows it with a very simple interface. That way people don't have to scour social media sites to get updates on the price multiple times a day, which is a challenge for people that are not tech savvy and/or have slow connection.</p>
+
+        <p>I&#39;m good at writing chronicles. As in, I-should-be-doing-that-for-a-living-instead-of-programming kind of good, with multiple publications in big sites like the NTY and the Washington Post.</p>
+        <p>I learned English by playing Zelda and Star Fox in the Nintendo 64, which officially makes me a Certified Nerd™.</p>
+        <p>I&#39;m very into metal music. I used to play the keyboard in <a href="https://soundcloud.com/neutrinia-banda">a power metal band</a> back in college. I kinda&#39; miss that.</p>
+        <p>I like exercising, specially running, although as a Certified Nerd™, I admit that screwing around with data sheets about my performance in Excel was half of the fun. Right now I&#39;m into CrossFit, which I&#39;m terrible at, but it&#39;s still lots of fun (what up 6 am squat?).</p>
+        <h2 id="publications">Publications</h2>
+        <p>Here are some notable publications:</p>
+        <p>“How Bitcoin Saved My Family”. February 23, 2019. New York Times. Op-ed
+          piece. Published
+          on the printed edition of the Sunday Review.</p>
+        <p>“On a Date While Venezuela Burns”. August 4, 2017. Op-ed piece. Published
+          on the printed edition of the Sunday Review.</p>
+        <p>“Binding Constraints, or why the Venezuelan Economy is Screwed”. Caracas
+          Chronicles. November 18, 2016. A review of a study by the Center for
+          International Development (CID) of Harvard Kennedy School about the
+          macroeconomic constrains for investments in Venezuela.</p>
+        <p>“How I Voted in Venezuela’s (Non) Election”. May 21, 2018. New York Times.
+          Op-ed piece.</p>
+        <p>“Middle Class and Hungry in Venezuela”. August 16, 2016. New York Times.
+          Op-ed piece.</p>
+        <p>&quot;In Venezuela, a daily struggle for the basic necessities of life”. April
+          10, 2018. Washington Post. Op-ed Piece on the Democracy Post section.</p>
+        <h2 id="open-source-projects">Open-source Projects</h2>
+        <p>For now this section is better read on <a href="https://carlos815.github.io/portfolio/">my portfolio page</a> which has some nice illustrations, and a handy graph of all of my tech stack. I&#39;m in the process of migrating all of that to this new blog, so this section will eventually be it&#39;s own page. For now, it&#39;s this very lame list.</p>
+        <h5 id="full-stack-online-shop-">Full stack online shop.</h5>
+        <p><a href="https://3rd-shop.vercel.app/">Demo</a> | <a href="https://github.com/carlos815/3rd-shop-frontend">Front-end source code</a> | <a href="https://github.com/carlos815/3rd-shop-backend">Back-end source code</a> | <a href="https://www.figma.com/file/9ggHbHWHxIXkcL0kUdF5Q0/3rd-Shop?node-id=9%3A241&amp;t=QSw2DVPTPc4OyzmR-1">Design</a></p>
+        <p>A fullstack project powered by Next.js and KeystoneJS. Supports account creation and credit card payments via Stripe. With full CMS support for managing products and accounts with ease. I did the design too 🦄</p>
+        <h5 id="ethereum-chat">Ethereum chat</h5>
+        <p><a href="https://eth-chat.vercel.app/">Demo</a> | <a href="https://github.com/carlos815/eth-chat">Source Code</a></p>
+        <p>A chat page with Metamask login built in Firebase. It lets you send messages to  other ethereum addresses.</p>
+        <h5 id="pomodoro-timer">Pomodoro Timer</h5>
+        <p><a href="https://tomate.netlify.app/">Demo</a> | <a href="https://github.com/carlos815/pomodoro">Source Code</a></p>
+        <p>A Pomodoro timer built in React using Redux. I designed it using neumorphism.</p>
 
         <h2>Contact</h2>
         <a href="https://www.linkedin.com/in/carlos-hern%C3%A1ndez-5a751422a/">LinkedIn</a>
--- src/pages/es/about.js
+++ src/pages/es/about.js
@@ -9,36 +9,70 @@ import Section from "../../components/section"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
-  const posts = data.allMarkdownRemark.nodes
 
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
+  const myAge = () => {
+    const myBirthDay = new Date("February 8, 1993 04:20:00")
+    const now = new Date()
+    const a = new Date(now - myBirthDay)
+    return (a.getFullYear() - 1970)
   }
+
+  React.useEffect(() => {
+    myAge()
+  }, [])
+
 
   return (
     <Layout location={location} title={siteTitle}>
 
       <Section>
-        <h1>About Me</h1>
+        <h1>¿Carlos Hernández? ¿Quién ejese?</h1>
 
-        <p className="text-base mb-4">So who the hell made this?. </p>
+        <p className="">Soy un desarrollador de Venezuela. Actualmente vivo en Colombia. Tengo {myAge()} años.</p>
 
-        <p className="text-base mb-4">
-          I'm a developer in Colombia. I make programming projects and then write about them. I am constantly learning things. I make music, practice chess, play videogames, write and read a lot.
+        <p className=" text-comment">
+          //De hecho soy un economista, pero aprendía a programar durante la pandemia
         </p>
 
+        <p>He trabajado con React, Flutter e incluso Python para algo de análisis de datos, pero principalmente me inclino por React.</p>
+        <p>Soy el creador de <a href="https://laslucas.com/m">Las Lucas</a>, un sitio web completamente gratuito que recopila los diferentes tipos de cambio en Venezuela. Tiene más de 10.000 visitas al mes.</p>
+        <p>El sitio utiliza múltiples web scrappers para mantenerse actualizado con el último precio de los distintos tipos de cambio oficiales (larga historia) y lo muestra con una interfaz muy sencilla. De esta manera, la gente no tiene que buscar en las redes sociales para obtener actualizaciones sobre el precio varias veces al día, lo que es un reto para las personas que no son conocedoras de tecnología y/o tienen una conexión lenta.</p>
+        <p>Se me da bien escribir crónicas. Nivel &quot;debería vivir de esto en lugar de programar&quot;, con múltiples publicaciones en grandes sitios como NTY y el Washington Post.</p>
+        <p>Aprendí inglés jugando a Zelda y Star Fox en el Nintendo 64, lo que oficialmente me convierte en un nerd certificado.</p>
+        <p>Me gusta el metal. Era el tecladista de <a href="https://soundcloud.com/neutrinia-banda">una banda de power metal</a> en la universidad.</p>
+        <p>Me gusta hacer ejercicio, sobre todo correr, aunque, como nerd que soy, admito que llevar los datos sobre mi rendimiento en Excel era la mitad de la diversión. Ahorita estoy en CrossFit, que soy malísimo, pero igual es entretenido.</p>
+        <h2 id="publicaciones">Publicaciones</h2>
+        <p>Aquí hay algunas de mis publicaciones destacadas:</p>
+        <p>“How Bitcoin Saved My Family”. 23 de febrero de 2019. New York Times. Artículo de opinión. Publicado
+          en la edición impresa del Sunday Review.</p>
+        <p>&quot;On a Date While Venezuela Burns&quot;. 4 de agosto de 2017. Artículo de opinión. Publicado
+          en la edición impresa del Sunday Review.</p>
+        <p>&quot;Binding Constraints, or why the Venezuelan Economy is Screwed&quot;. Caracas
+          Chronicles. 18 de noviembre de 2016. Reseña de un estudio del Centro para el
+          Desarrollo Internacional (CID) de la Harvard Kennedy School sobre las
+          limitaciones macroeconómicas para las inversiones en Venezuela.</p>
+        <p>&quot;How I Voted in Venezuela’s (Non) Election&quot;. 21 de mayo de 2018. New York Times.
+          Artículo de opinión.</p>
+        <p>&quot;Middle Class and Hungry in Venezuela&quot;. 16 de agosto de 2016. New York Times.
+          Artículo de opinión.</p>
+        <p>&quot;In Venezuela, a daily struggle for the basic necessities of life&quot;. Abril
+          10, 2018. Washington Post. Artículo de opinión en la sección Democracy Post.</p>
+        <h2 id="proyectos-de-c-digo-abierto">Proyectos open source</h2>
+        <p>Por ahora esta sección se lee mejor en <a href="https://carlos815.github.io/portfolio/">mi página de portafolio</a> que tiene ilustraciones, y un gráfico bien bueno de mi tech stack. Estoy en el proceso de migrar todo eso a este nuevo blog, por lo que esta sección más adelante será su propia página. Por ahora, es esta listica sencilla.</p>
+        <h5 id="tienda-online-full-stack-">Tienda online full stack.</h5>
+        <p><a href="https://3rd-shop.vercel.app/">Demo</a> | <a href="https://github.com/carlos815/3rd-shop-frontend">Código fuente front-end</a> | <a href="https://github.com/carlos815/3rd-shop-backend">Código fuente back-end</a> | <a href="https://www.figma.com/file/9ggHbHWHxIXkcL0kUdF5Q0/3rd-Shop?node-id=9%3A241&amp;t=QSw2DVPTPc4OyzmR-1">Diseño</a></p>
+        <p>Un proyecto fullstack hecho con Next.js y KeystoneJS. Soporta la creación de cuentas y pagos con tarjeta de crédito a través de Stripe. Viene con CMS para gestionar productos y cuentas con facilidad. Yo también hice el diseño 🦄.</p>
+        <h5 id="ethereum-chat">Ethereum chat</h5>
+        <p><a href="https://eth-chat.vercel.app/">Demo</a> | <a href="https://github.com/carlos815/eth-chat">Código Fuente</a></p>
+        <p>Una página de chat con login Metamask construido en Firebase. Te permite enviar mensajes a otras direcciones ethereum.</p>
+        <h5 id="temporizador-pomodoro">Temporizador Pomodoro</h5>
+        <p><a href="https://tomate.netlify.app/">Demo</a> | <a href="https://github.com/carlos815/pomodoro">Código Fuente</a></p>
+        <p>Un temporizador Pomodoro construido en React usando Redux. Lo diseñé usando neumorfismo.</p>
+
+        <h2>Contact</h2>
         <a href="https://www.linkedin.com/in/carlos-hern%C3%A1ndez-5a751422a/">LinkedIn</a>
         <a href="https://twitter.com/carlos_t815" className="mb-4">Twitter</a>
-        <p className="text-base mb-4">I also use the technology of internet mail correspondence, also known as <a href="https://en.wikipedia.org/wiki/Email">electronic mail</a>, contact me at cccarlos@duck.com      </p>
+        <p className="text-base mb-4">También soy un usuario de la tecnología de la correspondencia por internet, también conocida como <a href="https://en.wikipedia.org/wiki/Email">correo electrónico</a>: cccarlos@duck.com      </p>
       </Section>
     </Layout>
   )
