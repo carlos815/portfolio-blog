e932b3ee57c6bfbd8e9050676c154934939f545d
carlos815 <carlos_t815@outlook.com>
2023-01-28 14:26:45 -0500

    minor style changes

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -5,7 +5,7 @@ import * as React from "react"
 const Section = ({ children }) => {
 
     return (
-        <section className=" md:p-12 p-6 border-b relative page flex flex-col " >
+        <section className=" md:p-12 p-6 pb-0 md:pb-0  relative page flex flex-col " >
             {children}
 
         </section>
